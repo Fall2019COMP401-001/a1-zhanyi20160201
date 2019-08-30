b06780f77b8d9db80229719d31645b2547c67663
pc <pc@LAPTOP-D6V4HU15>
2019-08-31 00:23:05 +0800

    3rd

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,40 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int time=scan.nextInt();
+		String[] arr1=new String[time];
+		int[] arr2=new int[time];
+		int[] arr3=new int[time];
+	      for (int i=0;i<time;i++)
+		{
+			arr1[i]=scan.next();
+			scan.nextDouble();
+		}
+	      //
+        int person=scan.nextInt();
+		int item;int number;String name;
+		for(int i=0;i<person;i++) {
+	    	scan.next();scan.next();
+	    	item=scan.nextInt();
+	    			for(int j=0;j<item;j++) {
+	    		   number=scan.nextInt(); 
+	    		   name=scan.next();
+	    		   for(int k=0;k<time;k++) {
+	    		           if(arr1[k].equals(name))  
+	    		           {         
+	    		    	  arr2[k]+=number;
+	    		    	  arr3[k]++;
+	       		           }
+	    		   }
+	    	              }
+		}
+		for(int i=0;i<time;i++) {
+		if(arr3[i]==0) {
+			System.out.println("No customers bought "+arr1[i]);
+		}
+		else
+			System.out.println(arr3[i]+" customers bought "+arr2[i]+" "+arr1[i]);
+		}
 	}
 }
+
